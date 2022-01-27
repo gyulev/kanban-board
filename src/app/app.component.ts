371c936e0b83f727ed21b3c9625e1627e4e89e6b
Valentin Gyulev <valentingyulev@gmail.com>
2022-01-27 20:45:32 +0200

    edit and delete task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,9 +28,31 @@ export class AppComponent {
   inProgress: Task[] = [];
   done: Task[] = [];
 
-  editTask(list: string, task: Task): void {}
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult | undefined) => {
+        if (!result) {
+          return;
+        }
+        const dataList = this[list];
+        const taskIndex = dataList.indexOf(task);
+        if (result.delete) {
+          dataList.splice(taskIndex, 1);
+        } else {
+          dataList[taskIndex] = task;
+        }
+      });
+  }
 
-  drop(event: any): void {
+  drop(event: CdkDragDrop<any>): void {
     if (event.previousContainer === event.container) {
       return;
     }
@@ -55,7 +77,7 @@ export class AppComponent {
     dialogRef
       .afterClosed()
       .subscribe((result: TaskDialogResult | undefined) => {
-        if (!result) {
+        if (!result?.task?.description || !result?.task?.title) {
           return;
         }
         this.todo.push(result.task);
