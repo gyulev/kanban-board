d8451c64b9afb7313464be2241de71b7bfeee3b8
Valentin Gyulev <valentingyulev@gmail.com>
2022-01-27 12:21:23 +0200

    display toolbar and tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kanban-board';
+  todo: Task[] = [
+    {
+      title: 'Init project',
+      description: 'Create initial project files',
+    },
+    {
+      title: 'Add backend',
+      description: 'Implement server-side logic',
+    },
+  ];
 }
